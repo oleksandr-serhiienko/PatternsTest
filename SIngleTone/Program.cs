d6a062a072a807bc88b0b72c16275bf17f8dc6a9
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-22 11:44:42 +0200

    Example of singletone construction

--- SIngleTone/Program.cs
+++ SIngleTone/Program.cs
@@ -6,7 +6,7 @@ namespace SIngleTone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var db = SingletonDatabase.Instance;
         }
     }
 }
---
+++ SIngleTone/SingletonDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MoreLinq;
+
+namespace SIngleTone
+{
+    
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+
+            capitals = File.ReadAllLines("capitals.txt")
+                           .Batch(2)
+                           .ToDictionary(
+                            list => list.ElementAt(0).Trim(),
+                            list => int.Parse(list.ElementAt(1)));
+                           
+                           
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static SingletonDatabase instance = new SingletonDatabase();
+
+        public static SingletonDatabase Instance => instance;
+    }
+}
