1e9bd436fd76b6c159c979f74449bde439b6c0db
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-23 08:35:23 +0200

    added task

---
+++ SIngleTone/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIngleTone
+{
+    using System;
+
+    namespace Coding.Exercise
+    {
+        public class SingletonTester
+        {
+            public static bool IsSingleton(Func<object> func)
+            {
+                var obj1 = func.Invoke();
+                var obj2 = func.Invoke();
+                if (obj1.Equals(obj2))
+                    return true;
+                else
+                    return false;
+
+            }
+        }
+    }
+
+}
