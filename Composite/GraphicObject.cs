0593a23bd05c004ac85ea29ae68f90c46d868846
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-24 11:44:46 +0200

    tested composite design implementation

---
+++ Composite/GraphicObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public class GraphicObject
+    {
+        public string Color;
+        public virtual string Name { get; set; } = "Group";
+
+        private Lazy<List<GraphicObject>> children
+            = new Lazy<List<GraphicObject>>();
+
+
+        public List<GraphicObject> Children => children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+              .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color}")
+              .AppendLine(Name);
+
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+}
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -6,7 +6,17 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var drawing = new GraphicObject { Name = "My Drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow"});
+
+            var group = new GraphicObject();
+            group.Children.Add(new Square { Color = "Blue" });
+            group.Children.Add(new Circle { Color = "Blue" });
+
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
         }
     }
 }
