638c8d68dc95f79841367c98541e086d4cb3e91e
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-28 10:33:23 +0200

    testing users

--- FlyWeight/Program.cs
+++ FlyWeight/Program.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using JetBrains.dotMemoryUnit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FlyWeight
 {
+    [TestFixture]
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        [Test]
+        public void TestUser()//1655033
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User>();
+            foreach (var firstName in firstNames)
+                foreach (var lastName in lastNames)
+                    users.Add(new User($"{firstName} {lastName}"));
+
+            ForceGC();
+
+            dotMemory.Check(memory =>
+            {
+                Console.WriteLine(memory.SizeInBytes);
+            });
+        }
+
+
+        [Test]
+        public void TestUser2()//1655033 1296991
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User2>();
+            foreach (var firstName in firstNames)
+                foreach (var lastName in lastNames)
+                    users.Add(new User2($"{firstName} {lastName}"));
+
+            ForceGC();
+
+            dotMemory.Check(memory =>
+            {
+                Console.WriteLine(memory.SizeInBytes);
+            });
+        }
+        private void ForceGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private string RandomString()
+        {
+            Random rand = new Random();
+            return new string(
+                Enumerable.Range(0, 10).Select(i => (char)('a' + rand.Next(26)))
+                .ToArray());
         }
     }
 }
---
+++ FlyWeight/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyWeight
+{
+    public class User
+    {
+        private string fullName;
+        public User(string fullName)
+        {
+            this.fullName = fullName;
+        }
+    }
+
+}
