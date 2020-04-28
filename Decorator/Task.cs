05de068db9a59b989406a268c11b6fde89f96e2c
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-28 09:23:05 +0200

    completed the task

---
+++ Decorator/Task.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Bird
+    {
+        public int Age { get; set; }
+
+        public string Fly()
+        {
+            return (Age < 10) ? "flying" : "too old";
+        }
+    }
+
+    public class Lizard
+    {
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        Bird bird = new Bird();
+        Lizard Lizard = new Lizard();
+        int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                bird.Age = value;
+                Lizard.Age = value;
+            }
+        }
+
+        public string Fly()
+        {
+            return bird.Fly();
+        }
+
+        public string Crawl()
+        {
+            return Lizard.Crawl();
+        }
+    }
+}
