30448af911020282e03d6ef4cbfd5642c5b56697
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-29 09:53:39 +0200

    ffed property proxy example

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,17 +1,19 @@
-﻿using Proxy.ProtectionProxy;
+﻿using Proxy.PropertyProxy;
+using Proxy.ProtectionProxy;
 using System;
 
 namespace Proxy
 {
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            ICar car = new CarProxy(new Driver(12));
-            car.Drive();
+            //ICar car = new CarProxy(new Driver(12));
+            //car.Drive();
+
+            var c = new Creature();
+            c.Agility = 10;
+
         }
     }
 }
---
+++ Proxy/PropertyProxy/Creature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.PropertyProxy
+{
+    public class Creature
+    {
+        private Property<int> agility { get; set; } = new Property<int>();
+
+        public int Agility 
+        { 
+            get => agility.Value;
+            set => agility.Value = value;
+        }
+    }
+}
