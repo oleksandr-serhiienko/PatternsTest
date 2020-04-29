d55fb32ece02e42e9a370f2cc62255a614632eb7
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-29 15:17:09 +0200

    added TAsk

---
+++ Proxy/Task.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            return "drinking";
+        }
+
+        public string Drive()
+        {
+            return "driving";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "driving while drunk";
+        }
+    }
+
+    public class ResponsiblePerson
+    {
+        protected Person person;
+        public ResponsiblePerson(Person person)
+        {
+            this.person = person;
+        }
+
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            if (Age > 18)
+                return person.Drink();
+            else
+                return "too young";
+        }
+
+        public string Drive()
+        {
+            if (Age > 16)
+                return person.Drive();
+            else
+                return "too young";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "dead";
+        }
+    }
+}
