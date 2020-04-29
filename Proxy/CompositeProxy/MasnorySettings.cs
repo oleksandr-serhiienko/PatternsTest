9adeb33b28bf197409615b4ca3f49086354ec348
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-29 11:32:02 +0200

    example with the dynamic object proxy

--- Proxy/CompositeProxy/MasnorySettings.cs
+++ Proxy/CompositeProxy/MasnorySettings.cs
@@ -1,64 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Dynamic;
 using System.Text;
+using ImpromptuInterface;
+using static System.Console;
 
-namespace Proxy.CompositeProxy
+namespace DotNetDesignPatternDemos.Structural.Proxy
 {
-    public class MasnorySettings
+    public interface IBankAccount
     {
-        public bool? All
+        void Deposit(int amount);
+        bool Withdraw(int amount);
+        string ToString();
+    }
+
+    public class BankAccount : IBankAccount
+    {
+        private int balance;
+        private int overdraftLimit = -500;
+
+        public void Deposit(int amount)
         {
-            get 
+            balance += amount;
+            WriteLine($"Deposited ${amount}, balance is now {balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (balance - amount >= overdraftLimit)
             {
-                if (flags.Skip(1).All(f => f == flags[0]))
-                    return flags[0];
-                return null;
+                balance -= amount;
+                WriteLine($"Withdrew ${amount}, balance is now {balance}");
+                return true;
             }
-            set 
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+
+    public class Log<T> : DynamicObject where T : class, new()
+    {
+        private readonly T subject;
+        private Dictionary<string, int> methodCallCount =
+          new Dictionary<string, int>();
+
+        protected Log(T subject)
+        {
+            this.subject = subject ?? throw new ArgumentNullException(paramName: nameof(subject));
+        }
+
+        // factory method
+        public static I As<I>(T subject) where I : class
+        {
+            if (!typeof(I).IsInterface)
+                throw new ArgumentException("I must be an interface type");
+
+            // duck typing here!
+            return new Log<T>(subject).ActLike<I>();
+        }
+
+        public static I As<I>() where I : class
+        {
+            if (!typeof(I).IsInterface)
+                throw new ArgumentException("I must be an interface type");
+
+            // duck typing here!
+            return new Log<T>(new T()).ActLike<I>();
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            try
             {
-                if (!value.HasValue) return;
-                for (int i = 0; i < flags.Length; i++)
-                {
-                    flags[i] = value.Value;
-                }
+                // logging
+                WriteLine($"Invoking {subject.GetType().Name}.{binder.Name} with arguments [{string.Join(",", args)}]");
+
+                // more logging
+                if (methodCallCount.ContainsKey(binder.Name)) methodCallCount[binder.Name]++;
+                else methodCallCount.Add(binder.Name, 1);
+
+                result = subject.GetType().GetMethod(binder.Name).Invoke(subject, args);
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
             }
         }
-        //public bool? All
-        //{
-        //    get 
-        //    {
-        //        if (Pillars == Walls && Walls == Floors) return Pillars;
-        //        else return null;
-        //    }
-        //    set 
-        //    {
-        //        if (!value.HasValue) return;
-        //        Pillars = value.Value;
-        //        Walls = value.Value;
-        //        Floors = value.Value;
-        //    }
-        // }
 
-        //public bool Pillars, Walls, Floors;
-
-        private readonly bool[] flags = new bool[3];
-
-        public bool Pillars
+        public string Info
         {
-            get => flags[0];
-            set => flags[0] = value;
-        }
-        public bool Walls
-        {
-            get => flags[1];
-            set => flags[1] = value;
-        }
-        public bool Floors
-        {
-            get => flags[2];
-            set => flags[2] = value;
+            get
+            {
+                var sb = new StringBuilder();
+                foreach (var kv in methodCallCount)
+                    sb.AppendLine($"{kv.Key} called {kv.Value} time(s)");
+                return sb.ToString();
+            }
         }
 
+        // will not be proxied automatically
+        public override string ToString()
+        {
+            return $"{Info}{subject}";
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            //var ba = new BankAccount();
+            var ba = Log<BankAccount>.As<IBankAccount>();
+
+            ba.Deposit(100);
+            ba.Withdraw(50);
+
+            WriteLine(ba);
+        }
     }
 }
