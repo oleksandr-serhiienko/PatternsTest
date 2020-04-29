fb873f79a21bcd1d0aa40b6ef71880946897ad11
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-29 11:09:41 +0200

    example of the programm without array back prop

---
+++ Proxy/CompositeProxy/MasnorySettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.CompositeProxy
+{
+    public class MasnorySettings
+    {
+        public bool? All
+        {
+            get 
+            {
+                if (Pillars == Walls && Walls == Floors) return Pillars;
+                else return null;
+            }
+            set 
+            {
+                if (!value.HasValue) return;
+                Pillars = value.Value;
+                Walls = value.Value;
+                Floors = value.Value;
+            }
+         }
+
+        public bool Pillars, Walls, Floors;
+    }
+}
