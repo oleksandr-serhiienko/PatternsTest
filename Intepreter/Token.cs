875be397e60961921b3364f43cc19c9c6fc161eb
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-05-04 08:36:04 +0200

    added lexer for interpreter

---
+++ Intepreter/Token.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intepreter
+{
+    public class Token
+    {
+        public enum Type
+        {
+            Integer, Plus, Minus, Lparen, Pparen
+        }
+
+        public Type MyType;
+        public string Text;
+
+        public Token(Type myType, string text)
+        {
+            MyType = myType;
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"{Text}"; 
+        }
+    }
+}
