bb50525ef8f590a6695af67be025d19c48f389ef
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-05-04 14:38:13 +0200

    array backed properties

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Iterator
 {
     class Program
     {
+        public class Creature : IEnumerable<int>
+        {
+            private int strength = 0;
+            private int[] stats = new int[3];
+            public int Strenth
+            {
+                get => stats[strength];
+                set => stats[strength] = value;
+            }
+            public int Agility { get; set; }
+            public int Intelligence { get; set; }
+
+            public double AverageStat =>
+                stats.Average();
+
+            public IEnumerator<int> GetEnumerator()
+            {
+                return stats.AsEnumerable().GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public int this[int index]
+            {
+                get { return stats[index]; }
+                set { stats[index] = value;}
+            }
+        }
+
+
         static void Main(string[] args)
         {
             var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
