caae914d1aa07b8db1bca179b760cabc437d0607
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-05-12 10:29:37 +0200

    events for observer pattern

---
+++ Observer/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Observer
+{
+    public class Person
+    {
+        public void CatchACold()
+        {
+            Fallsill?.Invoke(this, new FallsillEventArgs { Address = "124 Kirovograd city"});
+        }
+
+        public event EventHandler<FallsillEventArgs> Fallsill;
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Fallsill += CallDoctor;
+            person.CatchACold();
+            person.Fallsill -= CallDoctor;
+        }
+
+        private static void CallDoctor(object sender, FallsillEventArgs e)
+        {
+            Console.WriteLine($"Doctor was cold to {e.Address}");
+        }
+    }
+}
