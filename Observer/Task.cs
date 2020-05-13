4b51649813ec163bf77ae28a89fdf6ecf1ae9916
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-05-13 10:15:30 +0200

    task to complete

---
+++ Observer/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Game
+    {
+        // todo
+        // remember - no fields or properties!
+    }
+
+    public class Rat : IDisposable
+    {
+        public int Attack = 1;
+
+        public Rat(Game game)
+        {
+            // todo
+        }
+
+
+        public void Dispose()
+        {
+            // todo
+        }
+    }
+}
+
