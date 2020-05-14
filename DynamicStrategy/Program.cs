8e13873a65820c8c356685b23fbaabb96fa3a248
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-05-14 09:26:50 +0200

    added dinamic strategy folder

---
+++ DynamicStrategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicStrategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
